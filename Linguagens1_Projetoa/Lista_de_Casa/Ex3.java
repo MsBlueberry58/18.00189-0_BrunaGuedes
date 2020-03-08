203f3498d5943f2e10a573bf2654ad67ec713cd5
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-03-08 20:36:39 -0300

    a

--- Linguagens1_Projetoa/Lista_de_Casa/Ex3.java
+++ Linguagens1_Projetoa/Lista_de_Casa/Ex3.java
@@ -5,28 +5,29 @@
 
 import java.util.Scanner;
 
-    class Ex3 {
-        public static void main (String[] args){
-            Scanner scanner = new Scanner(System.in);
-            String usuario, senha, usu2, sen2;
-            System.out.println("Informe seu usuario: ");
-            usuario = scanner.nextLine();
-            System.out.println("Informe sua senha: ");
-            senha = scanner.nextLine();
-            
-            System.out.println("Digite seu usuario: ");
-            usu2 = scanner.nextLine();
-            System.out.println("Digite sua senha: ");
-            sen2 = scanner.nextLine();
+class Ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String usuario, senha, usu2, sen2;
 
+        System.out.println("Informe seu usuario: ");
+        usuario = scanner.nextLine();
+        System.out.println("Informe sua senha: ");
+        senha = scanner.nextLine();
+
+        System.out.println("Digite seu usuario: ");
+        usu2 = scanner.nextLine();
+        System.out.println("Digite sua senha: ");
+        sen2 = scanner.nextLine();
+
+        if ((!usu2.equals(usuario)) || (!sen2.equals(senha))) {
             do {
                 System.out.println("Senha ou usuário incorretos. Tente novamente.");
                 System.out.println("Digite seu usuario: ");
                 usu2 = scanner.nextLine();
                 System.out.println("Digite sua senha: ");
                 sen2 = scanner.nextLine();
-            
-        } while ((!usu2.equals(usuario))||(!sen2.equals(senha)));
-
+            } while ((!usu2.equals(usuario)) || (!sen2.equals(senha)));
         }
     }
+}
