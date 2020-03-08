cf3a13ee89fd17021c9073175578fb90b45b72b6
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-03-08 20:31:45 -0300

    a

---
+++ Linguagens1_Projetoa/Lista_de_Casa/Ex3.java
@@ -0,0 +1,32 @@
+//Lista de exercícios
+//3. Construa um programa que pede para o usuário informar usuário e senha.
+//Depois seu programa deve continuar sendo executado até que o usuário
+//informe usuário e senha corretamente.
+
+import java.util.Scanner;
+
+    class Ex3 {
+        public static void main (String[] args){
+            Scanner scanner = new Scanner(System.in);
+            String usuario, senha, usu2, sen2;
+            System.out.println("Informe seu usuario: ");
+            usuario = scanner.nextLine();
+            System.out.println("Informe sua senha: ");
+            senha = scanner.nextLine();
+            
+            System.out.println("Digite seu usuario: ");
+            usu2 = scanner.nextLine();
+            System.out.println("Digite sua senha: ");
+            sen2 = scanner.nextLine();
+
+            do {
+                System.out.println("Senha ou usuário incorretos. Tente novamente.");
+                System.out.println("Digite seu usuario: ");
+                usu2 = scanner.nextLine();
+                System.out.println("Digite sua senha: ");
+                sen2 = scanner.nextLine();
+            
+        } while ((!usu2.equals(usuario))||(!sen2.equals(senha)));
+
+        }
+    }
