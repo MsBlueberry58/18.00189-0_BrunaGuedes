8a88b8de80daf5ba171fa2408a9f4b7ee89012d3
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-03-08 21:41:58 -0300

    a

--- Linguagens1_Projetoa/Lista_de_Casa/Ex5.java
+++ Linguagens1_Projetoa/Lista_de_Casa/Ex5.java
@@ -7,9 +7,19 @@ import java.util.Scanner;
     class Ex5 {
         public static void main (String[] args){
             Scanner scanner = new Scanner(System.in);
-            int val;
-
+            int val, i;
+            // O que significa e o que é inicializar valor?
+            int [] fib = 0;
+            
+            
             System.out.println("Digite o número do valor desejado: ");
             val = scanner.nextInt();
+            
+          for (i = 0; i<=(val+1); i++){
+              fib [0] = 1;
+              fib[i] = fib[i] + fib[i-1];
+          }
+
+            System.out.printf("O valor da casa %i na sequência de Fibonnacci é %i", val, fib[val+1]);
         }
     }
