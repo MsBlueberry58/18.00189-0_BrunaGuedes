3c14c8f87186394c39589c686d06942082eb3ffa
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-03-08 21:58:04 -0300

    a

--- Linguagens1_Projetoa/Lista_de_Casa/Ex6.java
+++ Linguagens1_Projetoa/Lista_de_Casa/Ex6.java
@@ -5,14 +5,30 @@
 
 import java.util.Scanner;
 
-    class Ex6 {
+    public class Ex6 {
         public static void main (String[] args){
-            Scanner scanner = new scanner (System.in);
-            int vali, valo;
+            
+            Scanner scanner = new scanner(System.in);
+            int vali, valo, i;
+            int soma = 0;
+            int mult = 1;
+
             System.out.println("Digite o valor inicial da sequência: ");
             vali = scanner.nextInt();
             System.out.println("Digite o valor final da sequência: ");
             valo = scanner.nextInt();
+
+            for (i = vali; i<=valo; i++){
+                soma = soma + i;
+                mult = mult * i;
+            }
+
+            System.out.println("Valor inicial: " + vali);
+            System.out.println("Valor final: " + valo);
+            System.out.println("Somatória: " + soma);
+            System.out.println("Produto: " + mult);
+
+
 
         }
     }
