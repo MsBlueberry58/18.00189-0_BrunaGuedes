701ea67e296769bae18057f9d381a0ed2f43bafe
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-03-09 10:59:12 -0300

    aaa

--- Linguagens1_Projetoa/Lista_de_Casa/Ex5.java
+++ Linguagens1_Projetoa/Lista_de_Casa/Ex5.java
@@ -4,19 +4,20 @@
 
 import java.util.Scanner;
 
-    class Ex5 {
+    public class Ex5 {
         public static void main (String[] args){
             Scanner scanner = new Scanner(System.in);
             int val, i;
+            int MAX = 50;
             // O que significa e o que é inicializar valor?
-            int [] fib = 0;
+            int [] fib = new int[MAX];
+            fib [0] = 1;
             
             
             System.out.println("Digite o número do valor desejado: ");
             val = scanner.nextInt();
             
-          for (i = 0; i<=(val+1); i++){
-              fib [0] = 1;
+          for (i = 1; i<=(val+1); i++){
               fib[i] = fib[i] + fib[i-1];
           }
 
