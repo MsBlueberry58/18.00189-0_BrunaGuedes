3ff4e63fb025239d2372a66bced34852a97dff44
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-03-23 10:38:41 -0300

    aula

--- Linguagens1_Projetos/Pratica03/Conta.java
+++ Linguagens1_Projetos/Pratica03/Conta.java
@@ -10,8 +10,8 @@ public class Conta(
         this.saldo += valor;
     }
 
-    public double consultaSaldo(){
-        return this.saldo;
-    }
+    public Conta (double saldoInicial){
+        this.saldo = saldoInicial;   
+     }
 
 )
--- Linguagens1_Projetos/Pratica03/Main.java
+++ Linguagens1_Projetos/Pratica03/Main.java
@@ -3,12 +3,12 @@ package com.company;
 
 public class Main{
     public static void main(String[] args){
-        Conta c1 = new Conta()
+        Conta c1 = new Conta(500);
         c1.depositar = 1000;
         System.out.println("Saldo Atual: " + c1.saldo);
-        c1.sacar(valor 200);
+        c1.sacar(200);
         System.out.println("Saldo Atual: " + c1.saldo);
-        c1.depositar(valor 1200);
+        c1.depositar(1200);
         System.out.println("Saldo Atual: " + c1.saldo);
 
     }
