df0050a62fc0f28ff305af31bc19b40e96606a2e
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-03-23 10:18:56 -0300

    aula

---
+++ Linguagens1_Projetos/Pratica03/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+public class Main{
+    public static void main(String[] args){
+        Conta c1 = new Conta()
+        c1.depositar = 1000;
+        System.out.println("Saldo Atual: " + c1.saldo);
+        c1.sacar(valor 200);
+        System.out.println("Saldo Atual: " + c1.saldo);
+        c1.depositar(valor 1200);
+        System.out.println("Saldo Atual: " + c1.saldo);
+
+    }
+
+}
