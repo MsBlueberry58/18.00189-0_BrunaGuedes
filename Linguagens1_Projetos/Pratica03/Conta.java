099d40eba86ccbc55b8be076dece14608b08974c
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-03-23 10:39:51 -0300

    bug

--- Linguagens1_Projetos/Pratica03/Conta.java
+++ Linguagens1_Projetos/Pratica03/Conta.java
@@ -1,17 +1,19 @@
 package com.company;
 
-public class Conta(
+public class Conta{
     private double saldo;
+
     public void sacar(double valor){
         if (saldo >= valor)
             saldo -= valor;
             }
-    public void depositar (double valor){
+
+    public void depositar(double valor) {
         this.saldo += valor;
     }
 
-    public Conta (double saldoInicial){
-        this.saldo = saldoInicial;   
-     }
+    public Conta(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
 
-)
+}
