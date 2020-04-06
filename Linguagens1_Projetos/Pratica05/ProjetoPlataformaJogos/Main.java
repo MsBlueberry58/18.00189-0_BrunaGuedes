fbee24656d45b9582b7f6ae94768fe92b9cb4c45
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-04-05 22:02:50 -0300

    ex 3 cont.

--- Linguagens1_Projetos/Pratica05/ProjetoPlataformaJogos/Main.java
+++ Linguagens1_Projetos/Pratica05/ProjetoPlataformaJogos/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
- 
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String exec = "1";
 
@@ -11,36 +10,35 @@ import java.util.Scanner;
         System.out.println("Criacao de conta\n");
         System.out.println("Digite um nick: ");
         nick = scanner.nextLine();
-        System.out.println("Crie uma senha: ");
+        Sy stem.out.println("Crie uma senha: ");
         senha = scanner.nextLine();
         System.out.println("Digite seu email: "); 
         email = scanner.nextLine();
         Usuario user = new Usuario(nick, senha, email);
-        Conta conta = new Conta (user);
+        Conta  conta = new Conta (user); 
         System.out.println("Nova conta criada com sucesso: \nUser: " + user.getNickname() + "\nConta: " + conta.getInfo());
 
-         // Login
-        System.out.println("Login\n");
-        String lognick, logpass;
+        // Login
+        String loguser, logpass;            
         do{
             System.out.println("Digite seu nick: ");
             loguser = scanner.nextLine();
             System.out.println("Digite sua senha: ");
-            logpass = scanner.nextLine();
+             logpass = scanner.nextLine();
 
         } while(!((loguser.equals(nick)) && (logpass.equals(senha))));
         
         // Menu
-    
-        do {
-        
         String choice;
+
+        do {
         System.out.println("Menu: \n1 - Trocar de usuario \n2 - Info conta \n3 - Adicionar jogos \n4 - Adicionar creditos \n5 - Criar outra conta \n6 - Sair\n ");
         choice = scanner.nextLine();
         
         // Trocar de usuario
         if(choice.equals("1")){
-           
+            exec.equals("1");
+
         }
 
         // Informações sobre conta atual
@@ -51,12 +49,42 @@ import java.util.Scanner;
 
         // Add jogos
         else if(choice.equals("3")){
-        
-            
+            String nome, genero, descricao, op;
+            Jogo jogo;
+            do{
+                System.out.println("Digite o nome do jogo: ");
+                nome = scanner.nextLine();
+                System.out.println("Digite o genero do jogo: ");
+                genero = scanner.nextLine();
+                System.out.println("Digite a descricao do jogo: ");
+                descricao = scanner.nextLine();
+                jogo = new Jogo(nome, genero, descricao);
+                conta.addJogo(jogo);
+                System.out.println("Deseja continuar adicionando? (s/n): ");
+                op = scanner.nextLine();
+
+            } while(op.equals("s"))
+           
+            System.out.println("Lista de jogos atual: " + conta.getJogos());
+            exec.equals("1");
+
         }
     
         // Add créditos
         else if(choice.equals("4")){
+            String op;
+            double quant;
+            System.out.println("Saldo atual: " conta.getCreditos());
+            do{
+                System.out.println("Digite a quantidade de creditos que deseja adicionar: ");
+                quant = scanner.nextDouble();
+                conta.addCreditos(quant);
+                System.out.println("Saldo atual: " + conta.getCreditos());
+                System.out.println("Deseja continuar adicionando? (s/n): ");
+                op = scanner.nextLine();
+            } while (op.equals("s"));
+            exec.equals("1");
+
             
         }
     
@@ -68,6 +96,11 @@ import java.util.Scanner;
         // Logout
         else if(choice.equals("6")){
             System.exit(0);
+        }
+
+        else{
+            System.out.println("Comando invalido! Tente novamente.\n")
+            exec.equals("1");
         }
     
         } while(exec.equals("1"));
