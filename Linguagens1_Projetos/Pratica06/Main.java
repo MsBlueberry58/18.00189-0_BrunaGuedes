d00b73bb82fe4fb1d83859487a85868d3aa2f78a
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-04-13 10:04:47 -0300

    mamis e filhos

---
+++ Linguagens1_Projetos/Pratica06/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Ninja ninja = new Ninja();
+        ninja.train();
+        AcademyStudent acadmeystudent = new AcademyStudent();
+        acadmeystudent.train();
+    }
+
+}
---
+++ Linguagens1_Projetos/Pratica06/Ninja.java
@@ -0,0 +1,11 @@
+
+public class Ninja {
+    private String name;
+    private String family;
+    private String jutsus;
+
+    public void train(){
+        System.out.println("Hoho!");
+    }
+
+}
