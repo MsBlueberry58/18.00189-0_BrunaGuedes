3a42f541c08ba3253333a1cf6a2f4695aeeba0c6
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-05-18 10:20:12 -0300

    teste packages

---
+++ Linguagens1_Projetos/Pratica10/ProjetoPoker/ProjetoPoker/game/gamecerto/src/app/PicturePoker.java
@@ -0,0 +1,7 @@
+package app;
+
+public class PicturePoker {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ Linguagens1_Projetos/Pratica10/ProjetoPoker/ProjetoPoker/game/models/src/app/PicturePokerCard.java
@@ -0,0 +1,7 @@
+package app;
+
+public class PicturePokerCard {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
