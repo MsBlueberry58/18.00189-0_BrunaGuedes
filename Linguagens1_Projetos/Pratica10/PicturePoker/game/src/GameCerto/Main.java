cf5d4241b4f665de50c646c4a139e353ec1a9468
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-05-18 11:44:10 -0300

    ESQUELETO

--- Linguagens1_Projetos/Pratica10/PicturePoker/game/src/GameCerto/Main.java
+++ Linguagens1_Projetos/Pratica10/PicturePoker/game/src/GameCerto/Main.java
@@ -1,7 +1,6 @@
-package app;
+package GameCerto;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+public class Main {
+    public static void main(String[] args){
     }
 }
