f74e91e169aa849365da0bd91e26bc306756c06b
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-06-08 11:20:22 -0300

    Correção pacote

--- Linguagens1_Projetos/Atividade_2/src/models/Funcionario.java
+++ Linguagens1_Projetos/Atividade_2/src/models/Funcionario.java
@@ -1,10 +1,10 @@
 package models;
 
-import funcionarios.User;
+import outros.User;
 
 public abstract class Funcionario extends User {
 
-    public Funcionario(String user, String email) {
+    public Funcionario(String user, enum funcao) {
         super(user, email);
     }
 
--- Linguagens1_Projetos/Atividade_2/src/num/Funcoes.java
+++ Linguagens1_Projetos/Atividade_2/src/num/Funcoes.java
@@ -1,5 +1,6 @@
 package num;
 
+
 public enum Funcoes {
     SCRIPT_GUYS, HEAVY_LIFTERS, MOBILE_MEMBERS, BIG_BROTHERS;
 
