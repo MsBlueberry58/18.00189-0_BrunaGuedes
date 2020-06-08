2ecd901d577fd99f78ef9f030ee2a609aa6fc549
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-06-08 10:34:04 -0300

    Configurando a main (menu)

--- Linguagens1_Projetos/Atividade_2/src/app/Main.java
+++ Linguagens1_Projetos/Atividade_2/src/app/Main.java
@@ -5,11 +5,35 @@
 
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
         System.out.println("Bem-vindo!");
+        int op;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(
+                    "Menu: \n 1- Cadastrar usuario \n 2 - Remover usuario \n 3 - Trocar horario de trabalho \n 4 - Postar mensagens \n 0 - Sair");
+            op = scanner.nextInt();
+            switch (op) {
+                case 1:
 
+                    break;
 
-        
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+
+            }
+
+        } while (op != 0);
     }
 }
