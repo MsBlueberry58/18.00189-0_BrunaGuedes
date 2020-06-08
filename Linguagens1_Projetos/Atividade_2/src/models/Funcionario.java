8b6b8f7839e64cbec9def70ad0baca1bb308372e
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-06-08 11:23:04 -0300

    Tchau usuario

--- Linguagens1_Projetos/Atividade_2/src/models/Funcionario.java
+++ Linguagens1_Projetos/Atividade_2/src/models/Funcionario.java
@@ -1,10 +1,11 @@
 package models;
 
+import num.Funcoes;
 import outros.User;
 
 public abstract class Funcionario extends User {
 
-    public Funcionario(String user, enum funcao) {
+    public Funcionario(String user, Funcoes funcao) {
         super(user, email);
     }
 
