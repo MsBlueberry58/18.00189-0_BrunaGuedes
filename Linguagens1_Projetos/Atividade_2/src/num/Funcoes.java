e53666b74a1d3c91bfe793722264e4af5d8e660f
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-06-08 14:00:32 -0300

    CORREÇÃO FINAL TOP

--- Linguagens1_Projetos/Atividade_2/src/num/Funcoes.java
+++ Linguagens1_Projetos/Atividade_2/src/num/Funcoes.java
@@ -1,6 +1,6 @@
 package num;
 
-/** Esse enum é usado na criação do HashMap, identificando os cargos dos funcionários */
+/** Esse enum é usado nos prints, identificando os cargos dos funcionários */
 
 public enum Funcoes {
     SCRIPT_GUYS, HEAVY_LIFTERS, MOBILE_MEMBERS, BIG_BROTHERS;
