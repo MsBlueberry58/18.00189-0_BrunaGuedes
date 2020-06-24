5aa7d1791eb6022d1cc689fd167ffbb3b20ece35
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-06-24 11:04:25 -0300

    Desenvolvendo o Pedido

--- Avaliacao_P1/src/dados_pedido/Pedido.java
+++ Avaliacao_P1/src/dados_pedido/Pedido.java
@@ -1,4 +1,25 @@
 package dados_pedido;
+import dados_pedido.enums.Estado_Pedido;
+
+import java.util.Random;
 
 public class Pedido {
+    String id;
+    Enum estado;
+    Enum pagamento;
+
+    public void Pedido() {
+        id = geradorID();
+        estado = Estado_Pedido.REALIZADO;
+    }
+
+    private String geradorID(){
+        Random random = new Random();
+        String idGerado = "";
+        for(int i = 0; i<3; i++){
+            idGerado += random.nextInt(10);
+        }
+        return idGerado;
+    }
+
 }
