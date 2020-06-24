0b9735f21f0bdaa656d95254ff2a3cbca8376e52
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-06-24 10:26:29 -0300

    Desenvolvendo o  menu

--- Avaliacao_P1/src/Main_Sistema.java
+++ Avaliacao_P1/src/Main_Sistema.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Main_Sistema {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem-vindo a Pizzaria Rato Que Ri!");
+        System.out.println("Escolha uma opcao:");
+        System.out.println("1 - Nova venda \n2 - Verificar pedidos \n3 - Alterar pedidos\n4 - Sair");
+        op = Integer.parseInt(scanner.nextLine());
+    }
+
 }
