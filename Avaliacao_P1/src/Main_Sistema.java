4d02a4785b77bb683c0d637d3e094c20dcce4fcf
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-06-24 10:36:54 -0300

    Criação da classe Usuário

--- Avaliacao_P1/src/Main_Sistema.java
+++ Avaliacao_P1/src/Main_Sistema.java
@@ -2,12 +2,37 @@ import java.util.Scanner;
 
 public class Main_Sistema {
     public static void main(String[] args) {
+        int op;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Bem-vindo a Pizzaria Rato Que Ri!");
-        System.out.println("Escolha uma opcao:");
-        System.out.println("1 - Nova venda \n2 - Verificar pedidos \n3 - Alterar pedidos\n4 - Sair");
-        op = Integer.parseInt(scanner.nextLine());
+
+        do {
+            System.out.println("Escolha uma opcao:");
+            System.out.println("1 - Nova venda \n2 - Verificar pedidos \n3 - Alterar pedidos\n4 - Sair");
+            op = Integer.parseInt(scanner.nextLine());
+
+            switch(op) {
+
+                case 1:
+
+
+                    break;
+
+                case 2:
+
+
+                    break;
+
+                case 3:
+
+
+                    break;
+
+            }
+
+        } while (op != 4);
+
     }
 
 }
---
+++ Avaliacao_P1/src/Usuario.java
@@ -0,0 +1,6 @@
+
+
+public class Usuario {
+    String nome;
+    String email;
+}
