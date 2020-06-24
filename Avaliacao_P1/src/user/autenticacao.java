06d9f0b92009ddbe8208ca77e1b2aad99d94c833
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-06-24 17:48:33 -0300

    Comentando em javadoc a interface autenticacao

--- Avaliacao_P1/src/user/autenticacao.java
+++ Avaliacao_P1/src/user/autenticacao.java
@@ -1,6 +1,15 @@
 package user;
 
+/**
+ * Essa interface é um contrato assinado por aqueles que podem ser autenticados pelo sistema principal
+ */
 public interface autenticacao {
 
+     /**
+      * Método que retorna um valor booleano de acordo com a verificação entre as senhas padrão e
+      * digitada pelo usuário
+      * @param senha digitada pelo usuário na main
+      * @return verdadeiro se as senhas baterem
+      */
      boolean autenticando (String senha);
 }
