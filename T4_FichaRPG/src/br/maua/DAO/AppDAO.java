4b4bff03aefd937741ffb20737e4bbccca5330e6
MsBlueberry58 <brunagguedes4@gmail.com>
2020-11-29 18:54:57 -0300

    Crição do primeiro caso do personagem

--- T4_FichaRPG/src/br/maua/DAO/AppDAO.java
+++ T4_FichaRPG/src/br/maua/DAO/AppDAO.java
@@ -50,23 +50,90 @@ public class AppDAO {
                 case 2:
                     System.out.println("Insira o nome do seu personagem: \n");
                     String nomePers = scanner.nextLine();
-                    System.out.println("Agora, escolha sua raça: \n1 - Humano \n2 - Orc\n 3 - Elfo");
+                    System.out.println("Agora, escolha sua raça: \n1 - Humano \n2 - Orc\n 3 - Elfo\n");
                     int op_race = Integer.parseInt(scanner.nextLine());
 
                     switch (op_race){
+
                         case 1:
+                            System.out.println("Qual sua profissao desejada?\n");
+                            System.out.println("1 - Mago \n2 - Cavaleiro \n");
+                            int op_prof_H = Integer.parseInt(scanner.nextLine());
+
+                            switch (op_prof_H){
+                                case 1:
+                                    System.out.println("Insira a sua mana: \n");
+                                    int H_M_mana = ((Integer.parseInt(scanner.nextLine()))+2);
+                                    System.out.println("Insira o seu ataque: \n");
+                                    int H_M_atk = ((Integer.parseInt(scanner.nextLine()))-2);
+                                    System.out.println("Insira o seu ataque magico: \n");
+                                    int H_M_atkm = Integer.parseInt(scanner.nextLine()) + 1;
+                                    System.out.println("Insira a sua defesa : \n");
+                                    int H_M_def = Integer.parseInt(scanner.nextLine()) - 1;
+                                    System.out.println("Insira a sua defesa magica: \n");
+                                    int H_M_defm = Integer.parseInt(scanner.nextLine()) + 1;
+                                    System.out.println("Insira a sua velocidade");
+                                    int H_M_vel = Integer.parseInt(scanner.nextLine()) - 1;
+                                    System.out.println("Insira a sua destreza");
+                                    int H_M_des = Integer.parseInt(scanner.nextLine());
+                                    System.out.println("Insira a sua experiencia");
+                                    int H_M_exp = Integer.parseInt(scanner.nextLine()) + 1;
+                                    System.out.println("Insira o seu nivel atual");
+                                    int H_M_lvl = Integer.parseInt(scanner.nextLine());
+
+                                    Personagem HumanoMago = new Personagem(nomePers, "Humano", "Mago", H_M_mana, H_M_atk, H_M_atkm, H_M_def, H_M_defm, H_M_vel, H_M_des, H_M_exp, H_M_lvl);
+
+                                    break;
+
+                                case 2:
+
+                                    break;
+
+                            }
+
 
                             break;
 
                         case 2:
+                            System.out.println("Qual sua profissao desejada?\n");
+                            System.out.println("1 - Mago \n2 - Cavaleiro \n");
+                            int op_prof_O = Integer.parseInt(scanner.nextLine());
+
+                            switch (op_prof_O){
+                                case 1:
+
+                                    break;
+
+                                case 2:
+
+                                    break;
+
+                            }
 
                             break;
 
                         case 3:
+                            System.out.println("Qual sua profissao desejada?\n");
+                            System.out.println("1 - Mago \n2 - Cavaleiro \n");
+                            int op_prof_E = Integer.parseInt(scanner.nextLine());
+
+                            switch (op_prof_E){
+                                case 1:
+
+                                    break;
+
+                                case 2:
+
+                                    break;
+
+                            }
 
                             break;
 
                     }
+
+
+
             }
         } while (rodar);
     }
